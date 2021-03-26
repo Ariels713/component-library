94cabfaacdfc8c7b727cefb501787ac79b2be2a9
Ariel Rodriguez <arielrodriguez@Ariels-Mac-mini.local>
2021-03-26 16:18:29 -0400

    working on code block styling

--- components/CodeSnippedDropDown.js
+++ components/CodeSnippedDropDown.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/dracula";
-export default function CodeSnippedDropDown({ codeSnippen, buttonTitle }) {
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+export default function CodeSnippedDropDown({ codeSnippet, buttonTitle }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [copied, setCopied] = useState(false);
   return (
     <div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
       <dl className="space-y-6 divide-y divide-gray-200">
@@ -48,10 +51,73 @@ export default function CodeSnippedDropDown({ codeSnippen, buttonTitle }) {
           <div className={`${isOpen ? "" : "hidden"}`}>
             <dd className="mt-2 " id="faq-0">
               <div className="bg-[#04293A] py-10 px-2 w-full rounded mt-4 relative">
+                <div className="relative">
+                  <CopyToClipboard text={codeSnippet}>
+                    <button onClick={() => setCopied(!copied)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        className="absolute -top-4 right-6"
+                      >
+                        <title>document-copy</title>
+                        <g stroke-width="2" fill="#d12594" stroke="#d12594">
+                          <path
+                            fill="none"
+                            stroke="#d12594"
+                            stroke-linecap="square"
+                            stroke-miterlimit="10"
+                            d="M2.66 3.8h16.72v19h-16.72z"
+                          ></path>
+                          <path
+                            fill="none"
+                            stroke-linecap="square"
+                            stroke-miterlimit="10"
+                            d="M4.94 1.52h16.72v19"
+                          ></path>
+                          <path
+                            fill="none"
+                            stroke-linecap="square"
+                            stroke-miterlimit="10"
+                            d="M5.7 8.74h10.64"
+                          ></path>
+                          <path
+                            fill="none"
+                            stroke-linecap="square"
+                            stroke-miterlimit="10"
+                            d="M5.7 11.78h10.64"
+                          ></path>
+                          <path
+                            fill="none"
+                            stroke-linecap="square"
+                            stroke-miterlimit="10"
+                            d="M5.7 14.82h10.64"
+                          ></path>
+                          <path
+                            fill="none"
+                            stroke-linecap="square"
+                            stroke-miterlimit="10"
+                            d="M5.7 17.86h4.18"
+                          ></path>
+                        </g>
+                      </svg>
+                    </button>
+                  </CopyToClipboard>
+                  <span
+                    className={`${
+                      copied
+                        ? "display absolute top-4 right-0 transform -skew-y-12 text-white font-semibold border-2 rounded-sm border-white text-base px-1 py-1"
+                        : "hidden"
+                    }`}
+                  >
+                    Copied
+                  </span>
+                </div>
                 <Highlight
                   {...defaultProps}
                   theme={theme}
-                  code={codeSnippen}
+                  code={codeSnippet}
                   language="jsx"
                 >
                   {({
--- components/buttons/80sButtons/SnesButton.js
+++ components/buttons/80sButtons/SnesButton.js
@@ -5,9 +5,6 @@ import CodeSnippedDropDown from "../../CodeSnippedDropDown";
 
 const buttonCode = `
 <div className="flex items-center justify-start">
-  <div className=" absolute bottom-[70px] bg-gray-800
-    opacity-10 rounded-sm shadow-lg w-[180px] h-[30px]">
-  </div>
   <button className="border-4 border-[#754c9a] 
     bg-[#8558ad] py-3 px-6 rounded-sm shadow-md  
     hover:shadow-2xl focus:outline-none focus:ring-2 
@@ -31,7 +28,6 @@ export default function SnesButton() {
       <div className="space-y-4 mt-4 text-gray-900 ">
         <p className="font-semibold text-xl">The NES Collection</p>
         <div className="flex items-center justify-start">
-          <div className=" absolute bottom-[70px] bg-gray-800 opacity-10   rounded-sm shadow-lg w-[180px] h-[30px]"></div>
           <button className=" border-4 border-[#754c9a] bg-[#8558ad] py-3 px-6 rounded-sm shadow-md  hover:shadow-2xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#8558ad] transform hover:-translate-y-8 transition duration-200">
             {" "}
             <span className="border-2 border-[#754c9a] border-opacity-50 rounded-full py-2 px-8 text-gray-200 font-semibold text-xl shadow-inner bg-[#8f65b3]">
@@ -41,7 +37,7 @@ export default function SnesButton() {
         </div>
       </div>
       <CodeSnippedDropDown
-        codeSnippen={buttonCode}
+        codeSnippet={buttonCode}
         buttonTitle={"SNES Button Code Snippet"}
       />
     </>
