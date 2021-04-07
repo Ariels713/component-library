31bd5a699eaccb5618ef0b0d30c29c66f0a5ac42
Ariel Rodriguez <arielrodriguez@Ariels-Mac-mini.local>
2021-04-07 07:06:47 -0400

    finished dark mode form

--- components/forms/RegistrationFormDark.js
+++ components/forms/RegistrationFormDark.js
@@ -6,11 +6,12 @@ export default function RegistrationFormDark() {
   const [org, setOrg] = useState("");
   const [city, setCity] = useState("");
   const [country, setCountry] = useState("");
-  console.log(name);
+
   return (
     <div className="flex items-center justify-center bg-gray-900 rounded-md mt-4 max-w-2xl mx-auto">
       <div className="flex-1 mx-auto">
         <div className="grid grid-cols-8">
+          {/* Left Column, will have form data supplied from state as the user fills out the form */}
           <div className=" h-96 w-full col-span-3 ">
             <div className="flex flex-col space-y-2 my-8 px-4   ">
               <h2 className="text-white font-extralight text-2xl mx-auto tracking-wide uppercase">
@@ -21,12 +22,12 @@ export default function RegistrationFormDark() {
                 welcoming you into our community.
               </p>
             </div>
-            <div className="flex flex-col skew-y-2 w-4/6 h-1/2  justify-start items-start mx-auto">
+            <div className="flex flex-col py-4 px-4  justify-start items-start mx-auto">
               <h3 className="text-[#F3A712] font-medium text-xl">{name}</h3>
               <p className="inline text-white font-light text-xs">{title}</p>
-              <span className="inline text-white font-extralight">
-                {!org ? "" : "|"}
-              </span>
+              <p className="text-xs text-white font-semibold uppercase">
+                {org}
+              </p>
             </div>
           </div>
           <div className=" h-96 w-full col-span-5 bg-gray-800">
@@ -95,7 +96,7 @@ export default function RegistrationFormDark() {
                         id="city"
                         className="bg-gray-700 border-transparent mt-1 block w-full rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm text-[#F3A712]"
                         value={city}
-                        onChange={(e) => set(e.target.value)}
+                        onChange={(e) => setCity(e.target.value)}
                       />
                     </div>
                     <div className="col-span-3 ">
